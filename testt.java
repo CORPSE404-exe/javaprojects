51a53ac416252c00b225c7a703c4009007049c79
CORPSE <shantikarmacharya1979@gmail.com>
2025-12-04 09:36:59 +0545

    Initial commit

--- testt.java
+++ testt.java
@@ -2,7 +2,7 @@ public class testt{
     public static void main(String[]args){
         for(int i = 0 ; i<4;i++){
             for(int j = 0 ; j<4; j++){
-                System.out.print("*");
+                System.out.print("1");
             }
             System.out.print("\n");
         }

deafb77674a5b8b3fcc23b002ab2f52e0d153dee
CORPSE <shantikarmacharya1979@gmail.com>
2025-12-04 09:20:01 +0545

    week 5

---
+++ test.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class test{
+    public static void main(String[]args){
+    System.out.println("Enter Your Grade: ");
+    Scanner a = new Scanner(System.in);
+    int x = a.nextInt();        
+    System.out.println("Your Grade: "+x);
+    }
+}
